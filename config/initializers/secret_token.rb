e709c611a0bcc50dbb8432182ef591692e376d26
Brandon Burkhardt <brburkhardt@gmail.com>
2012-12-10 02:09:38 -0500

    Iteration A2 - Making Things Prettier

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot2::Application.config.secret_token = 'a9749a268a359f452a21f120fcf576c72cf0d74bc8f1a4afb5d928a6862541fea513a3eec6e9f6d74f46e850cb08027b2a56150d99a8912fcffdb4261a157831'
